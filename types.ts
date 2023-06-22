5e477f62f49966e05e84e25192ae959b30ff9bd0
Wookie <bq1214@gmail.com>
2023-06-23 02:27:28 +0500

    feat!: move to next 13 app dir

--- types.ts
+++ types.ts
@@ -1,4 +1,14 @@
-export type Stats = {
+export type DiscordUser = {
+    id: string;
+    tag: string;
+    username: string;
+    discriminator: string;
+    avatarURL?: string;
+    bannerURL?: string;
+    accentHex?: string;
+}
+
+export type DiscordStats = {
     uptime: string;
     ram_usage: string;
     ws_ping: number;
