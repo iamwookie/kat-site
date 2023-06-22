faa681370111f8dd5a34f6eb951d1d85a115745a
Wookie <bq1214@gmail.com>
2023-06-23 02:33:19 +0500

    fix: another fix for invite route handler

--- app/support/route.ts
+++ app/support/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     try {
         const { instant_invite } = await fetch(process.env.DISCORD_WIDGET!).then((res) => res.json());
