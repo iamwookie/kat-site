0b253c7846b06ad153754b8ca28a14bf8ebddaa8
Wookie <bq1214@gmail.com>
2023-06-23 02:31:37 +0500

    fix: dynamic error on invite route

--- app/support/route.ts
+++ app/support/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-        const { instant_invite } = await fetch(process.env.DISCORD_WIDGET!, { next: { revalidate: 300 }}).then((res) => res.json());
+        const { instant_invite } = await fetch(process.env.DISCORD_WIDGET!).then((res) => res.json());
         if (!instant_invite) return new NextResponse('Not Found', { status: 404 });
         return NextResponse.redirect(instant_invite);
     } catch (err) {
